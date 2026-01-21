7178b7f0c7521e102b4aa7dcfd93e3f9cd3b08f4
niekboon <n.boon@justbetter.nl>
2026-01-21 09:26:25 +0100

    JBST-289 - Implemented first version of the Redirect middleware. Also added vue and vite because the code depends on them

--- src/Http/Middleware/RedirectIfNeeded.php
+++ src/Http/Middleware/RedirectIfNeeded.php
@@ -4,21 +4,54 @@ namespace JustBetter\Detour\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use JustBetter\Detour\Contracts\ResolvesRepository;
+use Statamic\Facades\Site;
 use Symfony\Component\HttpFoundation\Response;
 
 class RedirectIfNeeded
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        // logic by Niek
-        // ophalen detours huidige site en huidige path OF regex
-        // redirect naar detour
+        $repository = app(ResolvesRepository::class)->resolve();
+        $detours = $repository->all();
+
+        foreach ($detours as $detour) {
+            if (! $this->matchesRoute($detour['from'], $request->path(), $detour['type'])) {
+                continue;
+            }
+
+            if (! $this->includesCurrentSide($detour['sites'])) {
+                continue;
+            }
+
+            return redirect()->to($detour['to'], (int) $detour['code']);
+        }
 
         return $next($request);
+    }
+
+    protected function matchesRoute(string $from, string $currentPath, string $type): bool {
+        $normalizedPath = '/'.ltrim($currentPath, '/');
+
+        if ($type === 'regex') {
+            return $this->matchesPattern($from, $normalizedPath);
+        }
+
+        $normalizedFrom = '/' . ltrim($from, '/');
+
+        return $normalizedFrom === $normalizedPath;
+    }
+
+    protected function includesCurrentSide(array $sites): bool {
+        if (empty($sites)) {
+            return true;
+        }
+
+        $currentSiteHandle = Site::current()?->handle();
+        return in_array($currentSiteHandle, $sites, true);
+    }
+
+    protected function matchesPattern(string $pattern, string $currentPath): bool {
+        return preg_match($pattern, $currentPath) === 1;
     }
 }
